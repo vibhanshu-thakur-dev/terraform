dbc02a13f5644695cbbf3acfc3abb6c7423ea838
Vibhanshu Thakur <vibhanshur@yahoo.in>
2023-10-14 13:19:14 +0100

    change public route table association block to 1

--- v1/environment/main.tf
+++ v1/environment/main.tf
@@ -140,18 +140,12 @@ resource "aws_route_table" "public_subnet_route_table" {
 }
 
 
-# First public subnet being associated with the route table
+# Public subnet being associated with the route table
 resource "aws_route_table_association" "route_table_association_public_subnet_1" {
-  subnet_id      = element(aws_subnet.public_subnets.*.id, 0)
+  count = length(var.public_subnet_cidr_list)
+  subnet_id      = element(aws_subnet.public_subnets.*.id, count.index)
   route_table_id = aws_route_table.public_subnet_route_table.id
 }
-
-# Second public subnet being associated with the route table
-resource "aws_route_table_association" "route_table_association_public_subnet_2" {
-  subnet_id      = element(aws_subnet.public_subnets.*.id, 1)
-  route_table_id = aws_route_table.public_subnet_route_table.id
-}
-
 
 # Route tables for private subnets, two route tables will be created
 # one for each AZ
